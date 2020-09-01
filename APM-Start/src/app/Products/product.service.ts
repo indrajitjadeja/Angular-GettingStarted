bbc6268809b1bca72faf04ffcb3679e7a3ed88d6
indrajit.jadeja <indrajit.jadeja@eclinicalworks.com>
2020-09-01 10:30:36 +0530

    Chapter 12 Angular: Getting Started Completed Advance Routing and Navigation to Product detail page with reference to APM-Final html page.

--- APM-Start/src/app/Products/product.service.ts
+++ APM-Start/src/app/Products/product.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
-import { catchError,tap } from "rxjs/operators";
+import { catchError, tap, map } from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-productUrl:string = "api/products/products.json"
-  constructor(private http: HttpClient) {  }
+  productUrl: string = "api/products/products.json"
+
+  constructor(private http: HttpClient) { }
+
   getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(this.productUrl).pipe(
       tap(data => console.log(JSON.stringify(data))),
@@ -16,12 +18,18 @@ productUrl:string = "api/products/products.json"
     );
   }
 
-  private HandleError(err: HttpErrorResponse){
+  getProductById(id: number): Observable<IProduct> | undefined {
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => products.find(p => p.productId === id))
+    );
+  }
+
+  private HandleError(err: HttpErrorResponse) {
     let errorMessage = '';
     if (err.error instanceof ErrorEvent) {
       errorMessage = `Error Occurred: ${err.error.message}`;
     }
-    else{errorMessage = `Server returned code ${err.status}, error message is ${err.message}`;}
+    else { errorMessage = `Server returned code ${err.status}, error message is ${err.message}`; }
     console.error(errorMessage);
     return throwError(errorMessage);
   }
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { RouterModule, Router } from "@angular/router";
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './Products/product-list.component';
+import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpaces } from "./shared/convert-to-spaces.pipe";
 import { StarComponent } from "./shared/star.component";
 import { ProductDetailsComponent } from './products/product-details.component';
